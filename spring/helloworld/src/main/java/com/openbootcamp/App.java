1742db8df7a2ba1ba11d521c476b9466e3ce592e
hugo <hugo.garciac@edu.uah.es>
2023-01-01 15:56:33 +0100

    First commit/Base code

---
+++ spring/helloworld/src/main/java/com/openbootcamp/App.java
@@ -0,0 +1,13 @@
+package com.openbootcamp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
