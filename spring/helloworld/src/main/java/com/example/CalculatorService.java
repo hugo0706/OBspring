f4133670a489be199b241a2bb7d435b1d13c3d43
hugo <hugo.garciac@edu.uah.es>
2023-01-01 16:39:21 +0100

    primera leccion

---
+++ spring/helloworld/src/main/java/com/example/CalculatorService.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class CalculatorService {
+    public CalculatorService(){
+        System.out.println("CalculatorService constructor");
+    }
+    public String holaMundo(){
+        return "Hola mundo";
+    }
+}
